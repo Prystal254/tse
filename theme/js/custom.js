4803912113766901ba984326b5d74993eb025111
Ali Usman <ali.axecorp@gmail.com>
2023-07-04 05:22:45 +0500

    new wp

--- theme/js/custom.js
+++ theme/js/custom.js
@@ -162,15 +162,6 @@ $(window).on('resize scroll', function() {
               arrows: false,
               centerMode: true,
               centerPadding: '40px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
               slidesToShow: 1
             }
           }
